b602c604893e390b284ca4ea39660616a452c2c0
Max-leav <maxleavitt20@gmail.com>
2025-12-08 07:10:18 -0500

    added increased difficulty at night

--- game/scripts/GameManager.gd
+++ game/scripts/GameManager.gd
@@ -5,6 +5,7 @@ var challenge_active := false   # true when any zombie has engaged the player
 var current_island := 0
 var islands_unlocked := 1
 var current_day := 1
+var time = "day"
 var unlocked_islands = {
 	"Dock": true
 }
