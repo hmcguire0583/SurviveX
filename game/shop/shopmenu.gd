d5264c8945696e3b3e582d4f216fa009c1239a97
Max-leav <maxleavitt20@gmail.com>
2025-12-08 03:39:20 -0500

    changed collect to include amount collected

--- game/shop/shopmenu.gd
+++ game/shop/shopmenu.gd
@@ -86,7 +86,7 @@ func swap_item_forward():
 
 func buy():
 	var itemBought = items.filter(func(i): return i != null and i.name == animNames[item])
-	player.collect(itemBought[0])
+	player.collect(itemBought[0], 1)
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
