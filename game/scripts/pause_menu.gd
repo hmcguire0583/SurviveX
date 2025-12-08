28c1e11c27e7de089ceb27db9c34e5f0fc8b3882
João Ziedins <Joao>
2025-12-08 09:55:22 -0500

    right jp_gaem

--- game/scripts/pause_menu.gd
+++ game/scripts/pause_menu.gd
@@ -6,7 +6,6 @@ func _ready() -> void:
 	visible = false
 	get_tree().paused = false
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
